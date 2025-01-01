90cde5d9033876779b63b294c643706152c973fe
Sergey Maksimov <s.mksimov@gmail.com>
2025-01-01 21:08:00 +0700

    Add lazy.nvim

--- mynvim/init.lua
+++ mynvim/init.lua
@@ -1,5 +1,7 @@
 print("Hello, Shyam!")
 
+require("config.lazy")
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":.lua<CR>")
