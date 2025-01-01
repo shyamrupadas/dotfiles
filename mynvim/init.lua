dd552699a4ccc6fc0d048667af57e43a0ede0322
Sergey Maksimov <s.mksimov@gmail.com>
2025-01-01 21:34:56 +0700

    add ftplugin

--- mynvim/init.lua
+++ mynvim/init.lua
@@ -12,10 +12,10 @@ vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
