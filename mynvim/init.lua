ec6d514880a254bc49f2f02cc739f3353dd0a4b3
Sergey Maksimov <s.mksimov@gmail.com>
2025-01-02 19:23:43 +0700

    add treesitter and lua.lsp

--- mynvim/init.lua
+++ mynvim/init.lua
@@ -2,6 +2,9 @@ print("Hello, Shyam!")
 
 require("config.lazy")
 
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":.lua<CR>")
@@ -13,9 +16,9 @@ vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
---
+++ mynvim/lua/config/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+	ensure_installed = { "c", "typescript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+	-- What does it means?
+	auto_install = false,
+	-- What does it means?
+	-- ignore_install = { "javascript" },
+	highlight = {
+	  enable = true,
+	  disable = function(lang, buf)
+	    local max_filesize = 100 * 1024 -- 100 KB
+	    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	    if ok and stats and stats.size > max_filesize then
+	      return true
+	    end
+	  end,
+	  additional_vim_regex_highlighting = false,
+	},
+      }
+    end,
+  }
+}
