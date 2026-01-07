938b0e17d28ab18b59b9c4402e5abd6cae0a7db6
Sergey Maksimov <s.mksimov@gmail.com>
2026-01-07 08:04:20 +0700

    Add fixes to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
+vim.o.winborder = "rounded"
 vim.o.number = true
 vim.o.signcolumn = "yes"
 vim.o.wrap = false
-vim.o.tabstop = 4
+vim.o.tabstop = 2
 vim.g.mapleader = " "
-vim.o.winborder = "rounded"
 
 local map = vim.keymap.set
 
@@ -11,15 +11,32 @@ map('i', 'jk', '<ESC>')
 map('n', ';', ':')
 map('n', '<leader>o', ':update<CR> :source<CR>')
 map('n', '<leader>w', ':write<CR>')
+map('n', '<C>s', ':write<CR>') -- Проверить, работает ли
 map('n', '<leader>q', ':quit<CR>')
+
+map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = "https://github.com/mason-org/mason.nvim" },
 })
 
+-- Посмотреть канал TJ чтобы поставить автозавершение. Это работает в lua, но не работает в ts
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
+require "mason".setup()
 require "mini.pick".setup()
 require "oil".setup()
 
@@ -29,7 +46,16 @@ map('n', '<leader>e', ":Oil<CR>")
 map('n', '<leader>lf', vim.lsp.buf.format)
 
 
-vim.lsp.enable({ "lua_ls" })
+vim.lsp.enable({ "lua_ls", "ts_ls" })
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			}
+		}
+	}
+})
 
 
 vim.cmd("colorscheme vague")
