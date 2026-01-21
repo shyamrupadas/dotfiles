dbafdbce3c635d03f9b3a60cf020104fd4b6ece3
Sergey Maksimov <s.mksimov@gmail.com>
2026-01-21 22:00:56 +0700

    Add resize buffer keymaps

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<Esc>", { silent = true })
+
+vim.keymap.set("n", "<A-Left>", "<cmd>vertical resize -5<cr>", { desc = "Resize window left" })
+vim.keymap.set("n", "<A-Right>", "<cmd>vertical resize +5<cr>", { desc = "Resize window right" })
+vim.keymap.set("n", "<A-Up>", "<cmd>resize +5<cr>", { desc = "Resize window up" })
+vim.keymap.set("n", "<A-Down>", "<cmd>resize -5<cr>", { desc = "Resize window down" })
